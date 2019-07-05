d44adc21076d2b1f69e18f648d393d39991dbe7e
John Puddephatt <john@jdp.org.uk>
2019-07-05 19:08:05 +0100

    initial netlify cms setup

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(svgContents);
 
+  eleventyConfig.addPassthroughCopy('admin');
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('uploads');
 
